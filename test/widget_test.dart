369b8f4cdd29376ecfc5fa66389f79b8e7d6b50a
chandan <chandan101288@gmail.com>
2024-01-10 17:40:44 +0530

    Commit Changes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:myfirstflutterpro/activity/main.dart';
 
-import 'package:myfirstflutterpro/main.dart';
+
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
